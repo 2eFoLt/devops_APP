33f4cd4b6f2ab5e737beff35fe6931556fc78694
2eFoLt <grig2205@gmail.com>
2023-10-19 11:20:55 +0300

    clean_R

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import mysql.connector
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    config = {
+        'user': 'root',
+        'password': 'G7ue4$4/e6',
+        'host': '127.0.0.1',
+        'port': '3306',
+        'database': 'sales'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM k_contract')
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    for item in results:
+        print(item)
     return "okay, im here\n"
 
+
 if __name__ == '__main__':
-   app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
