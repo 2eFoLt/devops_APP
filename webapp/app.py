8a0988328560480ff736c860d356ca4ca26e7ad5
2eFoLt <grig2205@gmail.com>
2023-10-19 20:43:45 +0300

    make_user

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from db_config import db_config
 import mysql.connector
 
@@ -14,6 +14,23 @@ def index():
     cursor.close()
     connection.close()
     return results
+
+
+@app.route('/make_user', methods=['GET'])
+def make():
+    # http://127.0.0.1:5000/make_user?user=mike&email=mike@mail.com&pswd=mike
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    query = "INSERT INTO user (username,email,password) VALUES (%s, %s, %s)"
+    values = (request.args["user"], request.args["email"], request.args["pswd"])
+    try:
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(e)
+    cursor.close()
+    connection.close()
+    return request.args
 
 
 if __name__ == '__main__':
