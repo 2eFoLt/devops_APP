f2fc8fb2200200fc64d766f8df5453b0ced7fb6b
2eFoLt <grig2205@gmail.com>
2023-10-19 11:36:37 +0300

    compose+config

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,8 @@
 from flask import Flask
+from db_config import db_config
 import mysql.connector
 
 app = Flask(__name__)
-db_config = {
-    'user': 'root',
-    'password': 'root',
-    'host': 'db',
-    'port': '3306',
-    'database': 'sales'
-}
 
 
 @app.route('/', methods=['GET'])
---
+++ webapp/db_config.py
@@ -0,0 +1,7 @@
+db_config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'db',
+    'port': '3306',
+    'database': 'sales'
+}
