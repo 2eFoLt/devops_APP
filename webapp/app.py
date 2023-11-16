c3a374bde5bebcdef556f1017a67ee4afe1ac636
Esferaty <mcdiban@yahoo.com>
2023-11-16 17:45:01 +0300

    1

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from connection_tools import ConnectorDB
 
-
 app = Flask(__name__)
 connector_object = ConnectorDB()
 
@@ -9,23 +8,31 @@ connector_object = ConnectorDB()
 @app.route('/', methods=['GET'])
 def index():
     # TODO: на базе поменять название таблицы с groups -> ascend_groups, конфликт с служебными командами MYSQL
+    current_heights = connector_object.select(['*'], 'heights')
+    return render_template('index.html', heights=current_heights)
+
+
+@app.route('/view_height/<int:height_id>', methods=['GET'])
+def view_height(height_id):
     task1 = connector_object.select(['ascend_groups.group_name', 'ascend_groups.ascend_start_time'], 'ascend_stat',
                                     joins=['ascend_groups on ascend_stat.group_id = ascend_groups.id'],
-                                    where='height_id = 2',
+                                    where=f'height_id = {height_id}',
                                     order_by='ascend_groups.ascend_start_time')
-    task2 = connector_object.select(['*'], 'heights')
-    return render_template('index.html', task1=task1, task2=task2)
+    height = connector_object.select(['*'], 'heights', where=f'id={height_id}')
+    print(height)
+    return redirect(url_for('index'))
 
 
 @app.route('/create_height', methods=['GET', 'POST'])
 def create_height():
+    task2 = connector_object.select(['*'], 'heights')
     if request.method == 'POST':
         print(connector_object.insert('heights', ['name', 'height', 'country'], list(request.form.values())))
         return redirect(url_for('create_height'))
     else:
         heights = connector_object.select(['*'], 'heights')
         print(heights)
-    return render_template('index.html')
+    return render_template('create_height.html', task2=task2)
 
 
 @app.route('/date_time', methods=['GET', 'POST'])
