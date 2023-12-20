b0e6de88136d399d7b7648c33ec5c0a30a854138
2eFoLt <grig2205@gmail.com>
2023-12-21 01:58:49 +0300

    bandit_test

---
+++ bandit_parse.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+
+def check_report():
+    with open('test.json') as report:
+        json_rep = json.load(report)
+        if json_rep["metrics"]["_totals"]["SEVERITY.HIGH"] > 0:
+            print(json_rep["results"])
+            raise Exception("Severity check failed!")
+
+
+try:
+    check_report()
+except FileNotFoundError:
+    os.system('bandit -r webapp -n 3 -ii -lll -f json -o test.json')
+    check_report()
