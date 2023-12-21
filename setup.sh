fa2534af3eef80d60a76ba07f6e197f06070f010
2eFoLt <grig2205@gmail.com>
2023-12-21 04:20:22 +0300

    startup_shells

--- setup.sh
+++
@@ -1,3 +0,0 @@
-docker network create -d bridge devops_net || true
-docker run -d -p 5000:5000 --network devops_net 2efolt/devops_app
-docker run -d -h db --network devops_net 2efolt/devops_db
---
+++ startup/stop.sh
@@ -0,0 +1,3 @@
+docker stop devops_app
+docker stop devops_db
+docker system prune -a -f
