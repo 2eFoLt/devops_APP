ed7ee92a79dc071c8e33282de64d853c8c90a5f9
2eFoLt <82101829+2eFoLt@users.noreply.github.com>
2023-12-21 09:04:35 +0300

    Update db_config.py

--- webapp/db_config.py
+++ webapp/db_config.py
@@ -1,14 +1,7 @@
-# db_config = {
-#     'user': 'root',
-#     'password': 'root',
-#     'host': 'db',
-#     'port': '3306',
-#     'database': 'devops'
-# }
 db_config = {
     'user': 'root',
-    'password': 'G7ue4$4/e6',
-    'host': '127.0.0.1',
+    'password': 'root',
+    'host': 'db',
     'port': '3306',
-    'database': 'devops_app'
+    'database': 'devops'
 }
